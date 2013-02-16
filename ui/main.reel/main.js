d266b8f33c291a63ecff3a0d68ba662d441d1e8f
António Afonso <antonio.afonso@gmail.com>
2013-02-15 23:14:18 -0800

    Use the Loader component and a Main component
    Montage provides a Loader component that loads a specific component (by default main.reel) and waits until all dependencies are loaded before adding it to the document.
    
    This way there's no need to manually manage the "Loading..." text and the content of the application.

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -1,20 +1,37 @@
-/**
-    @module "ui/main.reel"
-    @requires montage
-    @requires montage/ui/component
-*/
-var Montage = require("montage").Montage,
-    Component = require("montage/ui/component").Component;
+// Module: main.js
+// Name: Main
+var Montage = require("montage/core/core").Montage;
+var Component = require("montage/ui/component").Component;
+var ArrayController = require("montage/ui/controller/array-controller").ArrayController;
+require('vendor/zepto');
+var _ = require('vendor/lodash');
+var Users = require('models/users').Users;
 
-/**
-    Description TODO
-    @class module:"ui/main.reel".Main
-    @extends module:ui/component.Component
-*/
-exports.Main = Montage.create(Component, /** @lends module:"ui/main.reel".Main# */ {
+exports.Main = Montage.create(Component, {
+    arrayController: {
+        value: null
+    },
 
-    thing: {
-        value: "World"
+    didCreate: {
+        value: function () {
+            this.users = new Users( { name:'default' } ); // give it one user by default
+            this.users.fetch(); // fetch users from the server, doesn't work right now--no resource!
+            var content = this.users.pluck('name');
+            this.arrayController = ArrayController.create().initWithContent(content);
+        }
+    },
+
+    handleAddButtonAction: {
+        value: function(event) {
+            var val = event.detail.username;
+            this.users.add( { name: val } );
+            this.arrayController.content = this.users.pluck('name');
+        }
+    },
+
+    handleSaveButtonAction: {
+        value: function(event) {
+            this.users.save();
+        }
     }
-
 });
